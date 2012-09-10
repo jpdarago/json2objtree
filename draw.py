c0dc9e1b27a48c6f91c358f68db92061af362263
Juan Pablo Darago <jpdarago@gmail.com>
2012-09-10 00:47:00 -0300

    Arreglado un bug con los dummies, sigue dibujando para abajo (maldito)

--- draw.py
+++ draw.py
@@ -6,7 +6,7 @@ import collections
 import sys
 import os.path
 import re
-
+import time
 
 #Arma las propiedades para hacer el digrafo
 def make_properties(shape,style="filled",fillcolor="yellow"):
@@ -18,7 +18,12 @@ def make_node(assertion_type, text):
 		'od': make_properties("parallelogram"),
 		'ob': make_properties("circle",style="filled",fillcolor="#aacccc"),
 		'ad': make_properties("trapezium"),
-		'ag': make_properties("hexagon")
+		'ag': make_properties("hexagon"),
+		'dummy': {
+			'shape': 'point',
+			'filled': 'filled',
+			'color': 'black'
+		}
 	}.get(assertion_type,dict(make_properties("plaintext")))
 	return pydot.Node(text, ** props )
 
@@ -42,13 +47,16 @@ class ObjectiveGraph(object):
 				return node[1]
 		return None
 
-	def add_node(self,assertion):
+	def add_node(self,assertion,append=False):
 		node = self.get_node(assertion)
-		if not node:
+		if not node or append:
 			node = make_node(*assertion)
 			self.__graph.add_node(node)
 			self.__node_dict.append((assertion,node))
 		return node
+
+	def add_relation_straight(self,child_node,parent_node,properties={}):
+		self.__graph.add_edge(pydot.Edge(child_node,parent_node, **properties))
 
 	def add_relation(self,child,parent,properties={}):
 		#Agregar ambas aserciones si no estan ya
@@ -56,18 +64,30 @@ class ObjectiveGraph(object):
 		parent_node = self.add_node(parent)
 
 		#Agregar el eje
-		self.__graph.add_edge(pydot.Edge(child.text,parent.text, **properties))
+		self.add_relation_straight(child_node,parent_node,properties)
 
 	def write(self):
-		return self.__graph.write_svg(self.name + '.svg')
+		self.__graph.write_dot(self.name + '.dot')
+		self.__graph.write_svg(self.name + '.svg')
+
+dummy_count = 0
 
 def build_tree(data,tree):
+	global dummy_count
+
 	#Estamos en un nodo.
 	root = Assertion(data['tipo'],data['texto'])
 	if 'y-ref' in data.keys():
+		dummy_node = tree.add_node(Assertion('dummy',"DUMMY" + str(dummy_count)),append=True)
+		dummy_count += 1
+
+		root_node = tree.add_node(root)
+
+		tree.add_relation_straight(dummy_node,root_node)
 		for o in data['y-ref']:
-			child = Assertion(o['tipo'],o['texto'])
-			tree.add_relation(child,root)
+			child_node = tree.add_node(Assertion(o['tipo'],o['texto']))
+		
+			tree.add_relation_straight(child_node,dummy_node, {'arrowhead': 'none'})
 
 			build_tree(o,tree)
 	elif 'o-ref' in data.keys():
@@ -96,6 +116,7 @@ def main():
 		print "Uso: %s [nombre archivo entrada]" 
 		sys.exit()
 
+	dummy_count = 0
 	with open(sys.argv[1], 'r') as f:
 		tree = ObjectiveGraph('objetivos')
 		build_tree(json.loads(f.read()), tree)
