323c5e0ebe9bc00f92bcffc2f71b94a2c235cd0e
Juan Pablo Darago <jpdarago@gmail.com>
2012-09-12 03:24:38 -0300

    Ahora duplica todo, hay que ver eso y agregarle para que dibuje sub arboles

--- draw.py
+++ draw.py
@@ -31,13 +31,10 @@ def make_properties(shape,style="filled",fillcolor="yellow",extra={}):
 	defaults = dict(zip(["shape","style","fillcolor"],[shape,style,fillcolor]))
 	
 	defaults.update(extra)
-	defaults.update({'width': '2.0'})
 	return defaults
 
 #Contador de la cantidad de nodos
 node_count = 0
-#Contador de nodos dummy para los y-refinamientos
-dummy_count = 0
 
 #Arma una asercion dado un texto 
 def make_node(assertion_type, text):
@@ -58,7 +55,7 @@ def make_node(assertion_type, text):
 		}
 	}.get(assertion_type,dict(make_properties("plaintext")))
 
-	w = textwrap.TextWrapper(width=60,break_long_words=False,replace_whitespace=False)
+	w = textwrap.TextWrapper(width=50,break_long_words=False,replace_whitespace=False)
 
 	props['label'] = '\n'.join( w.wrap( text ) )
 	node_count += 1
@@ -67,7 +64,7 @@ def make_node(assertion_type, text):
 class Assertion:
 	def __init__(self,type, text):
 		self.assertion_type = type
-		self.text = text
+		self.text = text.strip().capitalize()
 	
 	def __eq__(self,other):
 		if isinstance(other,Assertion): 
@@ -77,100 +74,96 @@ class Assertion:
 
 #Arbol de objetivos
 class ObjectiveGraph(object):
-	def __init__(self,name):
-		self.__name = name 
+	def __init__(self):
 		self.__graph = pydot.Dot('objetivos', graph_type='digraph')
-		self.__node_list = []
-
-	def get_name(self):
-		return self.__name
-
-	name = property(get_name)
-
-	def get_node(self,assertion):
-		for node in self.__node_list:
-			if assertion == node[0]:
-				return node[1]
-		return None
+		self.__nodes = []
 
 	def add_node(self,assertion):
-		node = self.get_node(assertion)
-		if not node:
-			node = make_node(assertion.assertion_type, assertion.text)
-			self.__graph.add_node(node)
-			self.__node_list.append((assertion,node))
-		return node
+		node = make_node(assertion.assertion_type, assertion.text)
 
-	def add_edge(self,child_node,parent_node,properties={}):
-		self.__graph.add_edge(pydot.Edge(child_node,parent_node, **properties))
+		self.__graph.add_node(node)
+		self.__nodes.append(node)
 
-	def add_relation(self,child,parent,properties={} ):
-		#Agregar ambas aserciones si no estan ya
-		child_node = self.add_node(child)
-		parent_node = self.add_node(parent)
+		return len(self.__nodes) - 1
+
+	def add_relation(self,child_index,parent_index,properties={} ):
+		if child_index < 0 or child_index >= len(self.__nodes):
+			raise Exception('Hijo invalido: %s' % child_index)
+
+		if parent_index < 0 or parent_index >= len(self.__nodes):
+			raise Exception('Padre invalido: %s' % root_index)
 
 		#Agregar el eje
-		self.add_edge(child_node,parent_node,properties)
+		self.__graph.add_edge(pydot.Edge(self.__nodes[child_index],self.__nodes[parent_index], **properties))
 
-	def write(self,extension):
-		self.__graph.write(self.name + '.' + extension, None, extension)
+	def write(self,output_file):
+		output_name, output_extension = os.path.splitext(output_file)
+		self.__graph.write(output_file, None, output_extension[1:])
 
-def add_impact(tree, root, soft_objs, edge_style):
+def add_impact(tree, root_index, soft_objs, edge_style):
 	"""
 		Agrega una arista de impacto sobre objetivo blando entre el nodo root
 		y los objetivos en soft_objs, con el estilo indicado
 	"""
 	for o in soft_objs:
-		soft_obj = Assertion('ob',o.strip().capitalize())
-		tree.add_relation(root,soft_obj, edge_style)
+		soft_obj_index = tree.add_node( Assertion('ob',o) )
+		tree.add_relation(root_index,soft_obj_index, edge_style)
 
-def build_tree(data,tree):
+def build_tree(data):
+	node_count = 0
+
+	tree = ObjectiveGraph()
+	root = Assertion( data['tipo'], data['texto'] )
+
+	build_branches(data,tree,tree.add_node(root))
+	return tree
+
+def build_branches(data,tree,root_index):
 	"""
 		Construye el arbol de objetivos (pasado por parametro como tree), a partir
 		del diccionario de datos dict, que tiene forma de json (ver example.json).
 	"""
 	global dummy_count
-	
-	node_text = data['texto'].strip().capitalize()
-	root = Assertion( data['tipo'], node_text )
 
 	if 'y-ref' in data.keys():
 		#El nodo dummy es el circulo negro que se usa para los y-refinamientos
-		dummy = Assertion('dummy',"DUMMY" + str(dummy_count))
-		dummy_count += 1
+		dummy_index = -1
+		
+		for o in data['y-ref']: 
+			child_index = tree.add_node(Assertion(o['tipo'],o['texto']))
 
-		add_dummy_edge = True
-		for o in data['y-ref']:
-			child = Assertion(o['tipo'],o['texto'])
-			t = o['tipo']
 			if len(data['y-ref']) == 1 or o['tipo'] == 'ag':
-				add_dummy_edge = False
-				tree.add_relation(root,child, 
+				tree.add_relation(root_index,child_index, 
 					{'arrowhead': 'none', 'arrowtail': 'none', 'dir': 'both'})
 			else:
-				tree.add_relation(dummy,child, {'arrowtail': 'none', 'dir': 'back'} )
+				if dummy_index == -1:
+					dummy = Assertion('dummy',"DUMMY")
+					dummy_index = tree.add_node(dummy)
 
-			build_tree(o,tree)
+				tree.add_relation(dummy_index,child_index, 
+					{'arrowtail': 'none', 'dir': 'back'} )
 
-		if add_dummy_edge:
-			tree.add_relation(root,dummy, {'dir': 'back'})
+			build_branches(o,tree,child_index)
+
+		if dummy_index != -1:
+			tree.add_relation(root_index,dummy_index, {'dir': 'back'})
 
 	elif 'o-ref' in data.keys():
 		for o in data['o-ref']:
-			child = Assertion(o['tipo'],o['texto'].strip().capitalize())
-			tree.add_relation(root,child, {'dir': 'back'})
+			child_index = tree.add_node(Assertion(o['tipo'],o['texto']))
+			tree.add_relation(root_index,child_index, {'dir': 'back'})
 
-			build_tree(o,tree)
+			build_branches(o,tree,child_index)
 
 	#Agregar los objetivos blandos que beneficia
 	helps = data.get('ayuda',[])
 	edge_style = {'label': '++', 'style': 'dotted', 'penwidth': 2}
-	add_impact(tree,root,helps,edge_style)
+	add_impact(tree,root_index,helps,edge_style)
 
 	#Agregar los objetivos blandos que dificulta
 	hardens = data.get('dificulta',[])
 	edge_style['label'] = '--'
-	add_impact(tree,root,hardens,edge_style)
+	add_impact(tree,root_index,hardens,edge_style)
 
 def main():
 	#Lee los argumentos de entrada estandar
@@ -186,21 +179,14 @@ def main():
 		name = 'a.svg' if args.input_file == '-' else args.input_file
 		args.output_file = os.path.splitext(name)[0] + '.svg'
 
-	output_name, output_extension = os.path.splitext(args.output_file)
-
 	try:
 		input_file = sys.stdin if args.input_file == '-' else open(args.input_file,'r')
 	except IOError as error:
 		print "Error %s: %s" % error
 
-	tree = ObjectiveGraph(output_name)
 	data = json.loads( input_file.read() )
-
-	dummy_count = 0 
-	node_count = 0
 	
-	build_tree(data, tree)
-	tree.write(output_extension[1:])
+	build_tree(data).write(args.output_file)
 
 if __name__ == '__main__':
 	main()
