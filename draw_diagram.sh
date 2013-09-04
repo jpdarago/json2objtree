c9bbe2fa9cc87e0e666b648f35d75c1cd2bc0ee0
Juan Pablo Darago <jpdarago@gmail.com>
2013-09-04 14:44:06 -0300

    Correcciones en el script de bash

--- draw_diagram.sh
+++ draw_diagram.sh
@@ -4,26 +4,26 @@ if [ $1 ]; then
 	./parser.py -i $1.jg -o $1.json
 
 	echo "Determinando branches"
-	branches=$(./draw.py -i $1.json -t)
+	branches=($(./draw.py -i $1.json -t))
 	files=""
 
 	dir=$(dirname $1)
 
 	echo "Los branches son $branches"
-	if [ -z "$branches"]; then
+	if [ "$branches" ]; then
+		for branch in "${branches[@]}"
+		do
+			echo "Procesando branch $branch"
+			./draw.py -i $1.json -o $branch.dot -r $branch
+			unflatten -f -l 5 $branch.dot -o $branch_unflatten.dot
+			dot -Tpdf $branch_unflatten.dot -o $branch.pdf
+			files=$files" $branch.pdf"
+		done
+		pdftk $files cat output $dir/diagrama.pdf
+	else
 		echo "No hay branches que procesar, al menos se debe tener un branch principal"
 		exit 1
 	fi
-
-	for branch in "${branches[@]}"
-	do
-		echo "Procesando branch $branch"
-		./draw.py -i $1.json -o $branch.dot -r $branch
-		unflatten -f -l 5 $branch.dot -o $branch_unflatten.dot
-		dot -Tpdf $branch_unflatten.dot -o $branch.pdf
-		files=$files" $branch.pdf"
-	done
-	pdftk $files cat output $dir/diagrama.pdf
 else
 	echo "Falta especificar el [archivo], ubicado en [archivo].jg"
 fi
