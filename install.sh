b12e7fd69f56316d31d8dbcb7ee628434f0cddab
Juan Pablo Darago <jpdarago@gmail.com>
2013-09-04 14:30:19 -0300

    Falta graphviz dev posiblemente

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get install graphviz graphviz-doc graphviz-dev
 
-sudo pip install pyparsing
+sudo pip uninstall pyparsing
+sudo pip install pyparsing==1.5.7
 sudo pip install pydot
